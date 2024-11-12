88b53f5eed42195048aa1c40d0d85fdb8d3a0dd2
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2024-11-12 13:53:42 +0530

    Add new file loop.cpp

---
+++ Basic C++/loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main( )
+{
+    // Pattern printing
+    // *
+    // **
+    // ***
+    // ****
+
+    int row;
+    cout<<"Enter the number of rows : ";
+    cin>>row;
+    for(int i = 0;i<row;i++){
+        for(int j = 0;j<=i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+
+  return 0;
+}
