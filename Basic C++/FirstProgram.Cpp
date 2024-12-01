1ea96ebb673f5395020cd84f59c9a11df3788ee8
Aditya Shee <161288779+AdityaShee12@users.noreply.github.com>
2024-12-01 19:56:27 +0530

    Add new C++ files

---
+++ Basic C++/FirstProgram.Cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    // Output
+    cout<<"This is my first Github repository."<<endl;
+
+    int a,b;
+    cout<<"Give me any two numbers and I will calculate the addition,subtraction,multiplication and division result"<<endl;
+    cout<<"First input : ";
+    cin>>a;
+    cout<<"First input : ";
+    cin>>b;
+    cout<<"Addition : "<<a+b<<endl;
+    cout<<"Subtraction : "<<a-b<<endl;
+    cout<<"Multiplication : "<<a*b<<endl;
+    cout<<"Division : "<<a/b<<endl;
+    return 0;
+}
